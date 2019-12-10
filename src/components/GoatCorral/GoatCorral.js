b46e47b6b0d6e2fe1d2f9f737ba959ff3644b209
Laura Collins <laura.e.collins1@gmail.com>
2019-12-09 19:47:18 -0600

    state props

---
+++ src/components/GoatCorral/GoatCorral.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class goatCorral extends React.Component {
+  render() {
+    const myGoats = this.props.butts;
+
+    return (
+      <div className="goatCorral">
+        Goat Corral
+      </div>
+    );
+  }
+}
+
+export default goatCorral;
