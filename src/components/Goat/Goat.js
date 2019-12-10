3a90050f55e7cebecd387298d014f0cc935f46db
Laura Collins <laura.e.collins1@gmail.com>
2019-12-09 20:41:34 -0600

    prop types

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -4,7 +4,7 @@ class Goat extends React.Component {
   render() {
     const { goat } = this.props;
     return (
-      <div className="card col-4">
+      <div className="card col-3 m-3">
         <img src={goat.imgUrl} className="card-img-top" alt={goat.name}/>
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
@@ -12,7 +12,7 @@ class Goat extends React.Component {
           <p className="card-text">{goat.description}</p>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Goat from '../Goat/Goat';
+import goatShape from '../../helpers/propz/goatShape';
 
 class goatCorral extends React.Component {
+  static propTypes = {
+    butts: PropTypes.arrayOf(goatShape.goatShape),
+  }
+
   render() {
     const myGoats = this.props.butts;
 
